5b4482d208b11e477129b282bd5cb978b7e6b290
Akash <akash@Akashs-MacBook-Pro.local>
2016-04-03 11:35:27 +0530

    initial commit

---
+++ src/common/handler_utils.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"github.com/gin-gonic/gin"
+	"db"
+)
+
+func BindResponse(c *gin.Context,obj interface{}) (err error) {
+	err = c.BindJSON(obj)
+	return err
+}
+
+func InsertDB(obj interface{}) (err error) {
+	dbPool := db.SharedConnection()
+
+	err = dbPool.Error
+	if err != nil {
+		return
+	}
+
+	err = dbPool.Create(obj).Error
+	return
+}
+
+func GetAllRows(obj []interface{},name string) {
+	dbPool := db.SharedConnection()
+	dbPool.Where("name=?",name).Find(&obj)
+}
---
+++ src/handler/product.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"product"
+	"common"
+)
+
+func FindProduct(c* gin.Context)  {
+	var products []product.Product
+	name := c.Param("name")
+	common.GetAllRows(products,name)
+}
+
+func CreateProduct(c *gin.Context) {
+	var product product.Product
+	common.BindResponse(c,product)
+
+}

0f794c0821c47f77a63b004d64cd4cfc257dc510
akashshinde <akashshinde159@gmail.com>
2016-05-08 15:35:34 +0530

    fixed doc issue

--- src/main/route.go
+++ src/main/route.go
@@ -19,11 +19,11 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.StaticFile("/swagger_api","../src/main/swagger.json")
+	// Serve static swagger pages for api docs
+	r.StaticFile("/swagger_api","swagger.json")
+	r.StaticFS("/docs",http.Dir("../../dist"))
 
-	r.StaticFS("/docs",http.Dir("../dist"))
-
-	// swagger:route GET /category listCategories category
+	// swagger:route GET /categories category
 	//
 	// Lists all available categories
 	//
@@ -39,15 +39,114 @@ func main() {
 	//       default: genericError
 	//       200: someResponse
 	//       422: validationError
-	r.POST("/category", handler.CreateCategory)
 	r.GET("/categories", handler.GetAllCategories)
 
-	r.POST("/gender", handler.CreateGender)
+	// swagger:route POST /category category
+	//
+	// Create new category
+	//
+	//     Consumes:
+	//     - application/json
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: http, https
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
+	r.POST("/category", handler.CreateCategory)
+
+	// swagger:route GET /genders gender
+	//
+	// Lists all available genders
+	//
+	// This will show all available genders by default.
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: http, https
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	r.GET("/genders", handler.GetAllGenders)
 
+	// swagger:route POST /gender gender
+	//
+	// Create new gender
+	// We added all available genders of planet earth, so please don't play with API ;)
+	//
+	//     Consumes:
+	//     - application/json
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: http, https
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
+	r.POST("/gender", handler.CreateGender)
+
+	// swagger:route POST /sub_category category
+	//
+	// Create new Sub Category
+	//
+	//     Consumes:
+	//     - application/json
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: http, https
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	r.POST("/sub_category", handler.CreateSubCategory)
 
+	// swagger:route POST /product product
+	//
+	// Create new Sub Product
+	//
+	//     Consumes:
+	//     - application/json
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: http, https
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	r.POST("/product", handler.CreateProduct)
+
+	// swagger:route POST /products product
+	//
+	// Create new Sub Product
+	//
+	//     Consumes:
+	//     - application/json
+	//
+	//     Produces:
+	//     - application/json
+	//
+	//     Schemes: http, https
+	//
+	//     Responses:
+	//       default: genericError
+	//       200: someResponse
+	//       422: validationError
 	r.GET("/products", handler.GetAllProducts)
 
 	r.POST("/user", handler.CreateUser)

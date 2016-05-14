5640e3ce41a33a8724a6b66f75628272ea4c1162
SUJAN <sujandt@gmail.com>
2016-05-14 15:42:31 +0530

    Added Test for Create gender
    Also added init and teardown function for testsuite
    @akash need to place in right file

--- src/handler/gender_test.go
+++ src/handler/gender_test.go
@@ -7,13 +7,39 @@ import (
 	"net/http/httptest"
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"os"
+	"db"
+	"product"
+	"strings"
 )
 
-func TestGetAllGenders(t *testing.T){
+//*********************************************
+//TestMain will get called For every test you run
+//-------Note: TODO: need to place in appropriate file
+func TestMain(m *testing.M) {
+	_Init();
+	result := m.Run();
+	_TearDown();
+	os.Exit(result);
+}
+
+func _Init() {
+	fmt.Println("Setting up test...\n");
+
+}
+
+func _TearDown() {
+	fmt.Println("Tearing down tests...\n");
+	fmt.Println("All testing stubs deleted successfully");
+}
+//-*******************************************
+
+
+func TestGetAllGenders(t *testing.T) {
 	gin.SetMode(gin.TestMode);
 	router := gin.Default();
 	router.GET("/genders", GetAllGenders)
-	req, err := http.NewRequest("GET", "/genders",nil)
+	req, err := http.NewRequest("GET", "/genders", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -21,4 +47,37 @@ func TestGetAllGenders(t *testing.T){
 	router.ServeHTTP(resp, req)
 	fmt.Println(resp.Body)
 	assert.Equal(t, resp.Code, 200)
+}
+
+func TestCreateGender(t *testing.T) {
+	gin.SetMode(gin.TestMode);
+	router := gin.Default();
+	router.POST("/gender", CreateGender);
+
+	testGenderJson := `{"id":9,"name":"TestGender"}`;
+	reader := strings.NewReader(testGenderJson)
+
+	req, err := http.NewRequest("POST", "/gender", reader);
+	if err != nil {
+		fmt.Println(err);
+	}
+	resp := httptest.NewRecorder();
+	router.ServeHTTP(resp, req);
+	dbpool := db.SharedConnection();
+	var testGender product.Gender;
+	err = dbpool.Find(&testGender, product.Gender{Id: 9}).Error;
+	if err != nil {
+		fmt.Println(err);
+	}
+
+	//*********************************************
+	//-----------Deleting test gender-------------
+	//*********************************************
+	err = dbpool.Delete(&testGender, product.Gender{Id:9}).Error;
+	if err != nil {
+		fmt.Println(err);
+	}
+	//----------------------------------------------
+
+	assert.Equal(t, resp.Code == 200 && testGender.Name == "TestGender", true);
 }

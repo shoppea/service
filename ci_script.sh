896be0623a373c130c3c2c4bc06385c0a0eff742
Akash Shinde <akashshinde159@gmail.com>
2016-06-15 20:19:43 +0530

    adding ci_script for deployment

---
+++ ci_script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+docker build -t snabar/api-service .
+echo "$(tput setaf 2)Building image $(tput sgr0)"
+docker run -d --name snabar-api-service -p 8081:8081 snabar/api-service
+echo "$(tput setaf 2)Running container for the first time $(tput sgr0)"
+
+if [ $? != 0 ]
+then
+    docker stop snabar-api-service
+    echo "$(tput setaf 1)Stopping Docker image $(tput sgr0)"
+    docker rm snabar-api-service
+    echo "$(tput setaf 1)Removing Docker image $(tput sgr0)"
+    docker run --rm --name snabar-api-service -p 8081:8081 -d snabar/api-service
+    echo "$(tput setaf 1)Starting new Docker image $(tput sgr0)"
+fi
+
+echo "$(tput setaf 2)Deployed Coming-Soon \n Please visit http://snabar.com $(tput sgr0)"

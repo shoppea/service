b2e63071b8f5b7ddd3774817faccb84218b54712
Akash Shinde <akashshinde159@gmail.com>
2016-06-16 09:32:09 +0530

    detaching api server docker image

--- ci_script.sh
+++ ci_script.sh
@@ -6,7 +6,7 @@ echo "$(tput setaf 2)Building image $(tput sgr0)"
     echo "$(tput setaf 1)Stopping Docker image $(tput sgr0)"
     docker rm snabar-api-service
     echo "$(tput setaf 1)Removing Docker image $(tput sgr0)"
-docker run --name snabar-api-service -p 8081:8081 snabar/api-service
+docker run --name snabar-api-service -d -p 8081:8081 snabar/api-service
 echo "$(tput setaf 2)Running container for the first time $(tput sgr0)"
 
 if [ $? != 0 ]
@@ -15,7 +15,7 @@ then
     echo "$(tput setaf 1)Stopping Docker image $(tput sgr0)"
     docker rm snabar-api-service
     echo "$(tput setaf 1)Removing Docker image $(tput sgr0)"
-    docker run --rm --name snabar-api-service -p 8081:8081 -d snabar/api-service
+    docker run -d --rm --name snabar-api-service -p 8081:8081 -d snabar/api-service
     echo "$(tput setaf 1)Starting new Docker image $(tput sgr0)"
 fi
 

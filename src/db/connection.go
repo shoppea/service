f650814ff05549cf8ac2a97c1815c0be0d285b70
Akash Shinde <akashshinde159@gmail.com>
2016-06-17 11:55:12 +0530

    changing db connection url

--- src/db/connection.go
+++ src/db/connection.go
@@ -17,6 +17,7 @@ var once sync.Once
 func SharedConnection() (*gorm.DB) {
 	once.Do(func() {
 		var err error
+		// Setting database endpoint
 		DBConnection,err = gorm.Open("mysql", "root:root@tcp(snabar.com:3306)/snabar_staging?parseTime=true")
 		//DBConnection, err = gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/snabar_staging")
 		DBConnection.LogMode(true)
